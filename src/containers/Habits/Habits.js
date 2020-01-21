accf582ed58b36a31eb0a8098b2c728362a4d843
Christopher <christopher@seekingwebdev.com>
2020-01-21 13:54:49 +0000

    send data from backend to frontend w hardcoded user

--- src/containers/Habits/Habits.js
+++ src/containers/Habits/Habits.js
@@ -1,26 +1,20 @@
-import React, {useState} from 'react'
-import Habit from '../../components/Habit/Habit'
+import React, { useState, useEffect } from "react";
+import Habit from "../../components/Habit/Habit";
 
 export default function Habits() {
-    const [habits, useHabits] = useState([
-        {
-            title: "This is a title",
-            completed: true,
-        },
-        {
-            title: "second title",
-            completed: false
-        },
-        {
-            title: "numero tres",
-            completed: true
-        }
-    ])
+
+    const [state, setState] = useState({habits: []})
+    useEffect(() => {
+        fetch("http://localhost:8081/users/5e259fee807dc62f6c96a3b6/habits")
+          .then(res => res.json())
+          .then((res)=> setState({habits: res}))
+        //   .then(res => console.log(res));
+      });
     return (
         <div>
             <p>This will have a list of habits!</p>
-            {habits.map((habit, index) => (
-              <Habit title={habit.title} completed={habit.completed}/>  
+            {state.habits.map((habit, index) => (
+              <Habit title={habit.title} completed={habit.completed}/>
             ))}
         </div>
     )
